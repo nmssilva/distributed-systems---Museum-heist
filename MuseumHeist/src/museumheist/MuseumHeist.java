1293157886f7be4499a52c75dfed44be6a82d589
Nuno Miguel Soares Silva <nuno.miguel.silva@ua.pt>
2017-03-09 10:11:58 +0000

    hehe

--- MuseumHeist/src/museumheist/MuseumHeist.java
+++ MuseumHeist/src/museumheist/MuseumHeist.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package museumheist;
+package MuseumHeist;
 
 import Logger.Log;
 import MasterThief.MasterThief;
