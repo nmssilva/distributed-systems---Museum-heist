efd42883d1083d7ff069bd3b84ebb9a0a23caa47
toipacoelho <toipacoelho@gmail.com>
2017-03-13 17:54:30 +0000

    logger

---
+++ MuseumHeist/src/GenRepOfInfo/Log.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GenRepOfInfo;
+
+import com.sun.javafx.binding.Logging;
+import Entities.MasterThiefState;
+import Entities.ThievesState;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * This log will be the gateway to all the information of the heist
+ * @author Pedro Coelho
+ * @author Nuno Silva
+ */
+public class Log {
+    
+}
